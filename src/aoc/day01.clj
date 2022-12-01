32f59fbf028db1fd1076bbfb64aafe1d195359af
Leonardo Gago <ljgago@gmail.com>
2022-12-01 11:55:03 -0300

    feat: day 1 done

--- src/aoc/day01.clj
+++ src/aoc/day01.clj
@@ -5,17 +5,25 @@
 ;; parse data
 (defn parse [input]
   (->>
-   (s/split-lines input)
-   (map #(Integer/parseInt %))
-   (reduce +)))
+   (s/split input #"\n\n")
+   (mapv #(s/split-lines %))
+   (mapv (fn [calories] (mapv #(Integer/parseInt %) calories)))))
 
 ;; part one
 (defn part1 [input]
-  (parse input))
+  (->>
+    (parse input)
+    (map #(reduce + %))
+    (apply max)))
 
 ;; part two
 (defn part2 [input]
-  (parse input))
+  (->>
+    (parse input)
+    (map #(reduce + %))
+    (sort >)
+    (take 3)
+    (reduce +)))
 
 ;; main
 (defn -main []
--- test/aoc/day01_test.clj
+++ test/aoc/day01_test.clj
@@ -2,19 +2,20 @@
   (:require [clojure.test :refer [deftest is testing]])
   (:require [aoc.day01 :as day01]))
 
-(def input "1")
+(def input "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000")
+(def parse_expected [[1000 2000 3000] [4000] [5000 6000] [7000 8000 9000] [10000]])
 
 (deftest parser
   (testing "test parser"
-    (let [expected 1]
+    (let [expected parse_expected]
       (is (= (day01/parse input) expected)))))
 
 (deftest part1
   (testing "test part 1"
-    (let [expected 1]
+    (let [expected 24000]
       (is (= (day01/part1 input) expected)))))
 
 (deftest part2
   (testing "test part 2"
-    (let [expected 1]
+    (let [expected 45000]
       (is (= (day01/part2 input) expected)))))

d50deb1e5aa958ca2b2b85af6288446c3fb7c15c
Leonardo Gago <ljgago@gmail.com>
2022-12-02 10:16:51 -0300

    chore: fix var name

--- test/aoc/day01_test.clj
+++ test/aoc/day01_test.clj
@@ -3,11 +3,11 @@
   (:require [aoc.day01 :as day01]))
 
 (def input "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000")
-(def parse_expected [[1000 2000 3000] [4000] [5000 6000] [7000 8000 9000] [10000]])
+(def parse-expected [[1000 2000 3000] [4000] [5000 6000] [7000 8000 9000] [10000]])
 
 (deftest parser
   (testing "test parser"
-    (let [expected parse_expected]
+    (let [expected parse-expected]
       (is (= (day01/parse input) expected)))))
 
 (deftest part1

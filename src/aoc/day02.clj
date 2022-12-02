cb4593373f36e623580022532af31815f5c64ec4
Leonardo Gago <ljgago@gmail.com>
2022-12-02 10:15:46 -0300

    feat: day 2 done

---
+++ src/aoc/day02.clj
@@ -0,0 +1,39 @@
+(ns aoc.day02
+  (:require [aoc.core :as core])
+  (:require [clojure.string :as s]))
+
+;; parse data
+(defn parse [input]
+  (->>
+   (s/split-lines input)
+   (mapv #(s/split % #" "))))
+
+;; part one
+(def strategy1 {["A" "X"] 4 ["A" "Y"] 8 ["A" "Z"] 3
+                ["B" "X"] 1 ["B" "Y"] 5 ["B" "Z"] 9
+                ["C" "X"] 7 ["C" "Y"] 2 ["C" "Z"] 6})
+
+(defn part1 [input]
+  (->>
+   (parse input)
+   (map #(get strategy1 %))
+   (reduce +)))
+
+;; part two
+(def strategy2 {["A" "X"] 3 ["A" "Y"] 4 ["A" "Z"] 8
+                ["B" "X"] 1 ["B" "Y"] 5 ["B" "Z"] 9
+                ["C" "X"] 2 ["C" "Y"] 6 ["C" "Z"] 7})
+
+(defn part2 [input]
+  (->>
+   (parse input)
+   (map #(get strategy2 %))
+   (reduce +)))
+
+;; main
+(defn -main []
+  (let [input (core/read-file "day02.txt")]
+    (println "--- Part One ---")
+    (println "Result:" (part1 input))
+    (println "--- Part Two ---")
+    (println "Result:" (part2 input))))
---
+++ test/aoc/day02_test.clj
@@ -0,0 +1,21 @@
+(ns aoc.day02-test
+  (:require [clojure.test :refer [deftest is testing]])
+  (:require [aoc.day02 :as day02]))
+
+(def input "A Y\nB X\nC Z")
+(def parse-expected [["A" "Y"] ["B" "X"] ["C" "Z"]])
+
+(deftest parser
+  (testing "test parser"
+    (let [expected parse-expected]
+      (is (= (day02/parse input) expected)))))
+
+(deftest part1
+  (testing "test part 1"
+    (let [expected 15]
+      (is (= (day02/part1 input) expected)))))
+
+(deftest part2
+  (testing "test part 2"
+    (let [expected 12]
+      (is (= (day02/part2 input) expected)))))

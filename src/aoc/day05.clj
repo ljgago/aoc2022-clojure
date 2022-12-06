c14afa8e082c90569c1c46460a696a2cc5502b50
Leonardo Gago <ljgago@gmail.com>
2022-12-06 19:57:40 -0300

    refactor: update day 5
    - Added parser for stacks.

--- src/aoc/day05.clj
+++ src/aoc/day05.clj
@@ -3,21 +3,21 @@
   (:require [clojure.string :as s]))
 
 ;; parse data
-(defn gen-map
-  "Generate the final hash-map of the stacks"
-  [matrix]
-  (->>
-   (mapv (fn [i] (mapv (fn [j] (str (get-in matrix [j i]))) (range 0 (count matrix)))) (range 0 (count (get matrix 0))))
-   (map-indexed (fn [i row] (hash-map (str (inc i)) (apply vector (reverse (filterv #(not (= % " ")) row))))))
-   (reduce #(conj %1 %2) {})))
-
 (defn parse-stacks [stacks]
   (let [matrix (pop (s/split-lines stacks))
         x-size (count (re-seq #"\d" (last (s/split-lines stacks))))]
     (->>
      matrix
-     (mapv (fn [line] (mapv #(nth line (inc (* % 4)) \space) (range 0 x-size))))
-     (gen-map))))
+     ;; gets characters in the positions 1, 5, 9, 13, etc
+     (mapv (fn [row] (mapv #(nth row (inc (* % 4)) \space) (range 0 x-size))))
+     ;; transposes the matrix and changes chars to strings
+     (apply mapv #(mapv str %&))
+     ;; removes the whitespaces and reverses the vector
+     (mapv (fn [row] (apply vector (reverse (remove #(= " " %) row)))))
+     ;; creates a hash-map with the index as key
+     (map-indexed (fn [i row] (hash-map (str (inc i)) row)))
+     ;; creates an unique hash-map
+     (reduce #(conj %1 %2) {}))))
 
 (defn parse-actions [actions]
   (->>
@@ -30,16 +30,14 @@
     [(parse-stacks stacks) (parse-actions actions)]))
 
 ;; part one
-(defn update-stack1 [action stacks]
-  (let [[amount orig dest] action
-        stack-orig (get stacks orig)
-        stack-dest (get stacks dest)]
-    (if (> amount 0)
-      (let [updated-stack-orig (pop stack-orig)
-            updated-stack-dest (conj stack-dest (last stack-orig))
-            updated-stacks (conj stacks (hash-map orig updated-stack-orig dest updated-stack-dest))]
-        (update-stack1 [(dec amount) orig dest] updated-stacks))
-      stacks)))
+(defn update-stack-1 [action stacks]
+  (let [[amount from to] action
+        stack-from (get stacks from)
+        stack-to (get stacks to)
+        updated-stack-from (apply vector (drop-last amount stack-from))
+        updated-stack-to (apply conj stack-to (reverse (take-last amount stack-from)))
+        updated-stacks (conj stacks (hash-map from updated-stack-from to updated-stack-to))]
+    updated-stacks))
 
 (defn get-message [stacks]
   (let [size (count stacks)]
@@ -48,23 +46,23 @@
 (defn part1 [input]
   (let [[stacks actions] (parse input)]
     (->>
-     (reduce #(update-stack1 %2 %1) stacks actions)
+     (reduce #(update-stack-1 %2 %1) stacks actions)
      (get-message))))
 
 ;; part two
-(defn update-stack2 [action stacks]
-  (let [[amount orig dest] action
-        stack-orig (get stacks orig)
-        stack-dest (get stacks dest)
-        updated-stack-orig (apply vector (drop-last amount stack-orig))
-        updated-stack-dest (apply conj stack-dest (take-last amount stack-orig))
-        updated-stacks (conj stacks (hash-map orig updated-stack-orig dest updated-stack-dest))]
+(defn update-stack-2 [action stacks]
+  (let [[amount from to] action
+        stack-from (get stacks from)
+        stack-to (get stacks to)
+        updated-stack-from (apply vector (drop-last amount stack-from))
+        updated-stack-to (apply conj stack-to (take-last amount stack-from))
+        updated-stacks (conj stacks (hash-map from updated-stack-from to updated-stack-to))]
     updated-stacks))
 
 (defn part2 [input]
   (let [[stacks actions] (parse input)]
     (->>
-     (reduce #(update-stack2 %2 %1) stacks actions)
+     (reduce #(update-stack-2 %2 %1) stacks actions)
      (get-message))))
 
 ;; main
